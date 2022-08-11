c04e1a3b37059741f089e96fc87a9eeb3978ed10
Luis Mv <luis1071@live.com.mx>
2022-08-11 14:05:16 -0500

    cambio de title

--- usuarios/ver_usuarios.php
+++ usuarios/ver_usuarios.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Ver || usuarios</title>
     <link rel="icon" type="image/png" href="../img/logo.png">
 
     <!--Import Google Icon Font-->
