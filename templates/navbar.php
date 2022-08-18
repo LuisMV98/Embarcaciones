a910bfd5c478d2531b558b0578f77c8f5f80dab6
Luis Mv <luis1071@live.com.mx>
2022-08-18 04:39:25 -0500

    mejoras

--- templates/navbar.php
+++ templates/navbar.php
@@ -12,6 +12,7 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 </head>
 
 <body>
